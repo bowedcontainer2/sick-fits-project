c38240daced16ac569699c823d469f8bbaacabd5
Matthew P <mpineda3247@gmail.com>
2019-12-15 21:41:46 -0800

    yayy progress: styled components and new front end things

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -8,6 +8,7 @@ const Meta = () => {
             <meta charSet="utf-8" />
             <link rel="shortcut icon" href="/static/favicon.png"/>
             <link rel="stylesheet" type="text/css" />
+            <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
             <title>Sick Fits!</title>
         </Head>
     );
